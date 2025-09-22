829667bc29cb16ff3388ad67b96d8f1f31981e5c
chunky152 <alvinmuwanguzi1@gmail.com>
2025-09-22 18:56:58 +0300

    initialised react

---
+++ hostel-booking/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hostel Booking System</h1>
+      <p>Welcome to the Hostel Booking System!</p>
+    </div>
+  );
+}
+
+export default App;
