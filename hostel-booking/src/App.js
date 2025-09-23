2b537bb5aaaf8b2f372822233759a86638005d9d
Muwanguzi Alvin Kiggundu <alvinmuwanguzi1@gmail.com>
2025-09-23 12:46:10 +0300

    added a button

--- hostel-booking/src/App.js
+++ hostel-booking/src/App.js
@@ -7,21 +7,7 @@ function App() {
       <p>Welcome to the Hostel Booking System!</p>
      <div>
       <p>Book your stay now!</p>
-      <form>
-        <label>
-          Name:
-          <input type="text" name="name" required />
-        </label>
-        <label>
-          Check-in Date:
-          <input type="date" name="checkin" required />
-        </label>
-        <label>
-          Check-out Date:
-          <input type="date" name="checkout" required />
-        </label>
-        <button type="submit">Book Now</button>
-      </form>
+      <button>Book Now</button>
      </div>
     </div>
   );
